8b38cad5446c3e3d0e63ff04de527f0263bd1c6d
Валентин <@DESKTOP-LOL3LJ6>
2017-03-14 14:28:06 +0300

    Добавьте файлы проекта.

---
+++ PingPongWindowsForms/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PingPongWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.aPanel1 = new System.Windows.Forms.PictureBox();
+            this.aPanel2 = new System.Windows.Forms.PictureBox();
+            this.aBall = new System.Windows.Forms.PictureBox();
+            this.aTimer = new System.Windows.Forms.Timer(this.components);
+            this.aBallTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.aPanel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aPanel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aPanel1
+            // 
+            this.aPanel1.Image = global::PingPongWindowsForms.Properties.Resources.tr_panel;
+            this.aPanel1.Location = new System.Drawing.Point(12, 174);
+            this.aPanel1.Name = "aPanel1";
+            this.aPanel1.Size = new System.Drawing.Size(27, 90);
+            this.aPanel1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.aPanel1.TabIndex = 0;
+            this.aPanel1.TabStop = false;
+            // 
+            // aPanel2
+            // 
+            this.aPanel2.Image = global::PingPongWindowsForms.Properties.Resources.tr_panel;
+            this.aPanel2.Location = new System.Drawing.Point(624, 174);
+            this.aPanel2.Name = "aPanel2";
+            this.aPanel2.Size = new System.Drawing.Size(27, 90);
+            this.aPanel2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.aPanel2.TabIndex = 1;
+            this.aPanel2.TabStop = false;
+            // 
+            // aBall
+            // 
+            this.aBall.BackColor = System.Drawing.Color.White;
+            this.aBall.BackgroundImage = global::PingPongWindowsForms.Properties.Resources.ball1;
+            this.aBall.Image = global::PingPongWindowsForms.Properties.Resources.ball1;
+            this.aBall.Location = new System.Drawing.Point(297, 174);
+            this.aBall.Name = "aBall";
+            this.aBall.Size = new System.Drawing.Size(31, 33);
+            this.aBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.aBall.TabIndex = 2;
+            this.aBall.TabStop = false;
+            // 
+            // aTimer
+            // 
+            this.aTimer.Enabled = true;
+            this.aTimer.Interval = 1;
+            this.aTimer.Tick += new System.EventHandler(this.aTimer_Tick);
+            // 
+            // aBallTimer
+            // 
+            this.aBallTimer.Enabled = true;
+            this.aBallTimer.Interval = 10;
+            this.aBallTimer.Tick += new System.EventHandler(this.aBallTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PingPongWindowsForms.Properties.Resources.ball1;
+            this.pictureBox1.Location = new System.Drawing.Point(240, 224);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(377, 183);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 81);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PingPongWindowsForms.Properties.Resources.pitch;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(663, 425);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.aBall);
+            this.Controls.Add(this.aPanel2);
+            this.Controls.Add(this.aPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.aPanel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aPanel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox aPanel1;
+        private System.Windows.Forms.PictureBox aPanel2;
+        private System.Windows.Forms.PictureBox aBall;
+        private System.Windows.Forms.Timer aTimer;
+        private System.Windows.Forms.Timer aBallTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ PingPongWindowsForms/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        const int movementSpeed = 3, top = 0, bottom = 333;
+        bool isUpPressed, isDownPressed;
+        int ballSpeedX = 3, ballSpeedY = 1;
+        int numberOfTicksUpDirection=0, numberOfTicksDownDirection;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //pictureBox3.Controls.Add(pictureBox1);
+            //pictureBox3.Location = new Point(0, 0);
+            aBall.BackColor = Color.Transparent;
+            aPanel1.BackColor = Color.Transparent;
+           // aPanel2.BackColor = Color.Transparent;
+            //pictureBox1.BackColor = Color.Transparent;
+
+        }
+        private void aBallTimer_Tick(object sender, EventArgs e)
+        {
+            aPanel2.Location = new Point(aPanel2.Location.X,Math.Max(0, Math.Min( aPanel2.Location.Y + (ballSpeedY), bottom)));
+            //aBall.Location = new Point(aBall.Location.X + ballSpeedX, aBall.Location.Y + ballSpeedY);
+            pictureBox1.Location = new Point(pictureBox1.Location.X - ballSpeedX, pictureBox1.Location.Y + ballSpeedY);
+
+            if (pictureBox1.Location.Y <= top || pictureBox1.Location.Y >= (bottom + aPanel2.Height-pictureBox1.Height))
+            {
+                ballSpeedY *= -1;
+            }
+
+           //else if (aPanel1.Bounds.Contains(pictureBox1.Bounds) || aPanel2.Bounds.Contains(pictureBox1.Bounds))
+           // {
+           //     ballSpeedX *= 1;
+           // }
+
+            if(aBall.Location.Y <= top || aBall.Location.Y >= (bottom+aPanel1.Height-aBall.Height))// || aPanel2.Location.Y>=bottom || aPanel2.Location.Y<=top)
+            {
+                ballSpeedY *= -1;
+
+            }
+            if(aPanel1.Bounds.IntersectsWith(pictureBox1.Bounds) || aPanel2.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                ballSpeedX *= -1;
+            }
+
+            if (pictureBox2.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                // ballSpeedX += 1;
+                ballSpeedY += 1;
+            }
+            if (aPanel1.Bounds.IntersectsWith(aBall.Bounds) || (aPanel2.Bounds.IntersectsWith(aBall.Bounds)))
+            {
+                ballSpeedX *= -1;
+            }
+            //if ((aBall.Location.X - aPanel1.Location.X)*(aBall.Location.X - aPanel1.Location.X) < 2  && (aPanel1.Location.Y-aBall.Location.Y)*(aPanel1.Location.Y - aBall.Location.Y) < 2)
+            //{
+            //    ballSpeedX *= -1;
+            //}
+
+
+            //if (((aBall.Location.Y-aPanel1.Location.Y)^2 + (aBall.Location.X - aPanel1.Location.X)^2) <4)
+            //{
+            //    ballSpeedX *= -1;
+            //}
+            //if (aPanel2.Location.X == aBall.Location.X)
+            //{
+            //    ballSpeedX *= -1;
+            //}
+
+        }
+        private void aTimer_Tick(object sender, EventArgs e)
+        {
+            //if (isUpPressed == true & isDownPressed == true)
+            //while (isUpPressed & isDownPressed)
+            //{
+            //    aPanel1.Location = new Point(aPanel1.Location.X, aPanel1.Location.Y);
+            //    {
+            //        if (!isUpPressed  || !isDownPressed )
+            //            break;
+            //    }
+            //}
+                
+            if (isUpPressed)
+            {
+                
+                aPanel1.Location = new Point(aPanel1.Location.X,
+                    Math.Max(top, aPanel1.Location.Y - (movementSpeed + numberOfTicksUpDirection/7))
+                        );
+                numberOfTicksUpDirection++;
+            }  else
+                numberOfTicksUpDirection = 0;
+
+           
+
+            if (isDownPressed)
+            {
+                aPanel1.Location = new Point(aPanel1.Location.X,
+                   Math.Min(aPanel1.Location.Y + movementSpeed + numberOfTicksDownDirection/7, bottom)
+                    );
+                numberOfTicksDownDirection++;
+            }
+            else numberOfTicksDownDirection = 0;
+
+           
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up)
+            { 
+                isUpPressed = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            { 
+              isUpPressed = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = false;
+            }
+        }
+    }
+}
