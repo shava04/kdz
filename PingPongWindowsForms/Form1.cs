3441a3ab737b40e004f4cd890faa1a53fee39c76
Валентин <@DESKTOP-LOL3LJ6>
2017-05-09 18:57:52 +0300

    обновленный старый проект

--- PingPongWindowsForms/Form1.cs
+++ PingPongWindowsForms/Form1.cs
@@ -13,114 +13,55 @@ namespace PingPongWindowsForms
 
     public partial class Form1 : Form
     {
-        const int movementSpeed = 3, top = 0, bottom = 333;
+        const int movementSpeed = 3, top = 0, bottom = 567, rightOfTheField = 983, leftOfTheField = 0;
         bool isUpPressed, isDownPressed;
-        int ballSpeedX = 3, ballSpeedY = 1;
+        int ballSpeedX = 6, ballSpeedY = 3;
         int numberOfTicksUpDirection=0, numberOfTicksDownDirection;
+        int n = 0;
 
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.
+        }
 
         public Form1()
         {
+
             InitializeComponent();
-            //pictureBox3.Controls.Add(pictureBox1);
-            //pictureBox3.Location = new Point(0, 0);
-            aBall.BackColor = Color.Transparent;
-            aPanel1.BackColor = Color.Transparent;
-           // aPanel2.BackColor = Color.Transparent;
-            //pictureBox1.BackColor = Color.Transparent;
+            
 
         }
         private void aBallTimer_Tick(object sender, EventArgs e)
         {
-            aPanel2.Location = new Point(aPanel2.Location.X,Math.Max(0, Math.Min( aPanel2.Location.Y + (ballSpeedY), bottom)));
-            //aBall.Location = new Point(aBall.Location.X + ballSpeedX, aBall.Location.Y + ballSpeedY);
-            pictureBox1.Location = new Point(pictureBox1.Location.X - ballSpeedX, pictureBox1.Location.Y + ballSpeedY);
+            
+           // Ball.Location = new Point(Ball.Location.X - ballSpeedX, Ball.Location.Y + ballSpeedY);
 
-            if (pictureBox1.Location.Y <= top || pictureBox1.Location.Y >= (bottom + aPanel2.Height-pictureBox1.Height))
+            if (Ball.Location.Y <= top || Ball.Location.Y >= (bottom -  Ball.Height - 25))
             {
                 ballSpeedY *= -1;
             }
-
-           //else if (aPanel1.Bounds.Contains(pictureBox1.Bounds) || aPanel2.Bounds.Contains(pictureBox1.Bounds))
-           // {
-           //     ballSpeedX *= 1;
-           // }
-
-            if(aBall.Location.Y <= top || aBall.Location.Y >= (bottom+aPanel1.Height-aBall.Height))// || aPanel2.Location.Y>=bottom || aPanel2.Location.Y<=top)
-            {
-                ballSpeedY *= -1;
-
-            }
-            if(aPanel1.Bounds.IntersectsWith(pictureBox1.Bounds) || aPanel2.Bounds.IntersectsWith(pictureBox1.Bounds))
+            if (Ball.Location.X <= leftOfTheField || Ball.Location.X >= rightOfTheField - Ball.Width)
             {
                 ballSpeedX *= -1;
             }
-
-            if (pictureBox2.Bounds.IntersectsWith(pictureBox1.Bounds))
-            {
-                // ballSpeedX += 1;
-                ballSpeedY += 1;
-            }
-            if (aPanel1.Bounds.IntersectsWith(aBall.Bounds) || (aPanel2.Bounds.IntersectsWith(aBall.Bounds)))
-            {
-                ballSpeedX *= -1;
-            }
-            if (pictureBox1.Location.X<0)
-            {
-                pictureBox1.Location = new Point(340, bottom / 2);
-                ballSpeedX = 3;
-                ballSpeedY = 1;
-            }
-            //if ((aBall.Location.X - aPanel1.Location.X)*(aBall.Location.X - aPanel1.Location.X) < 2  && (aPanel1.Location.Y-aBall.Location.Y)*(aPanel1.Location.Y - aBall.Location.Y) < 2)
-            //{
-            //    ballSpeedX *= -1;
-            //}
-
-
-            //if (((aBall.Location.Y-aPanel1.Location.Y)^2 + (aBall.Location.X - aPanel1.Location.X)^2) <4)
-            //{
-            //    ballSpeedX *= -1;
-            //}
-            //if (aPanel2.Location.X == aBall.Location.X)
-            //{
-            //    ballSpeedX *= -1;
-            //}
+            Ball.Location = new Point(Ball.Location.X + ballSpeedX, Ball.Location.Y + ballSpeedY);
 
         }
         private void aTimer_Tick(object sender, EventArgs e)
         {
-            //if (isUpPressed == true & isDownPressed == true)
-            //while (isUpPressed & isDownPressed)
-            //{
-            //    aPanel1.Location = new Point(aPanel1.Location.X, aPanel1.Location.Y);
-            //    {
-            //        if (!isUpPressed  || !isDownPressed )
-            //            break;
-            //    }
-            //}
-                
-            if (isUpPressed)
-            {
-                
-                aPanel1.Location = new Point(aPanel1.Location.X,
-                    Math.Max(top, aPanel1.Location.Y - (movementSpeed + numberOfTicksUpDirection/7))
-                        );
-                numberOfTicksUpDirection++;
-            }  else
-                numberOfTicksUpDirection = 0;
 
-           
-
-            if (isDownPressed)
+            n += 1;
+            if (n % 2 == 0)
             {
-                aPanel1.Location = new Point(aPanel1.Location.X,
-                   Math.Min(aPanel1.Location.Y + movementSpeed + numberOfTicksDownDirection/7, bottom)
-                    );
-                numberOfTicksDownDirection++;
+                Ball.ImageLocation = "C:\\Users\\Валентин\\Desktop\\Soccerball.png";
             }
-            else numberOfTicksDownDirection = 0;
+            if (n % 2 == 1)
+            {
+                Ball.ImageLocation = "C:\\Users\\Валентин\\Desktop\\maball.png";
+            }
 
-           
+
+
 
 
         }
